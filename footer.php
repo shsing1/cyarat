83b4cde9850625ff95a72af82ec2ee47a100cf33
shsing1 <shsing999@gmail.com>
2013-10-16 10:50:47 +0800

    youtube刪除

--- footer.php
+++ footer.php
@@ -9,10 +9,9 @@ $footer_info['desc'] = str_replace('../images/editor_upload', './images/editor_u
             <div class="footer_copyright">© 2013 新竹藝動節. All Rights Reserved.</div>
             <div class="share">
                 <a target="_blank" href="https://www.facebook.com/pages/2013%E6%96%B0%E7%AB%B9%E8%97%9D%E5%8B%95%E7%AF%80-%E5%9F%8E%E5%B8%82%E5%9C%B0%E6%99%AF%E8%97%9D%E8%A1%93%E5%B1%95/575660252470323?ref=hl">
-                <img src="images/default/icon_fb.png"></a>
+                <img src="images/default/icon_fb.png"></a>&nbsp;
                 <a target="_blank" href="https://www.facebook.com/SuperheroIGotU">
                 <img src="images/default/icon_fb2.png"></a>
-                <img src="images/default/icon_yt.png">
             </div>
             <?php echo $footer_info['desc'];?>
         </div>
